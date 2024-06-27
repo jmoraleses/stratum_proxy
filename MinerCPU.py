b3a14ccf030f0e6b5485ace34d51cceaec6918b4
javi <javiermoralesestevez@gmail.com>
2024-06-27 15:42:21 +0200

    update

--- MinerCPU.py
+++ MinerCPU.py
@@ -1,18 +1,17 @@
 import configparser
 import hashlib
 import logging
+import multiprocessing
 import random
 import signal
 import struct
 import sys
 import threading
 import time
-from concurrent.futures import ThreadPoolExecutor
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 import pandas as pd
 import requests
 from BlockTemplate import BlockTemplate
-from DatabaseHandler import DatabaseHandler
 from sha256_opencl import sha256_pyopencl
 
 NUM_ZEROS = 15
@@ -20,26 +19,27 @@ NUM_ZEROS = 15
 
 class MinerCPU:
     def __init__(self, config):
-        self.db_handler = DatabaseHandler(config.get('DATABASE', 'db_file')).create_table()
         self.stratum_processing = StratumProcessing(config, self)
-        self.generated_merkle_roots = []
         self.valid_merkle_count = 0
         self.merkle_counts_file = config.get('FILES', 'merkle_file')
         self.merkle_counts = self.load_merkle_counts(self.merkle_counts_file)
-        self.job_queue = []
-        self.merkle_roots = []
-        self.merkle_roots_done = []
+        manager = Manager()
+        self.job_queue = manager.list()
+        self.generated_merkle_roots = manager.list()
+        self.merkle_roots = manager.list()
+        self.merkle_roots_done = manager.list()
 
     def block_template_updater(self):
         while True:
             self.stratum_processing.block_template_fetcher.fetch_template()
             time.sleep(2)
 
-    def merkle_root_counter(self):
-        wait = 10
+    def merkle_root_counter(self, job_queue, generated_merkle_roots, merkle_roots):
+        wait = 60
         time.sleep(wait)
         while True:
-            self.count_valid_merkle_roots(wait)
+            self.count_valid_merkle_roots(wait, job_queue, generated_merkle_roots, merkle_roots)
+
             time.sleep(wait)
 
     def load_merkle_counts(self, file_path):
@@ -55,30 +55,24 @@ class MinerCPU:
             return self.merkle_counts['merkle_root'].apply(lambda root: merkle_root.startswith(root)).any()
         return False
 
-    def count_valid_merkle_roots(self, wait):
-        # valid_count = sum(self.check_merkle_root(root) for root in self.generated_merkle_roots)
-
-        total_generated = len(self.generated_merkle_roots)
-
-        # Calcula el hashrate en hashes por segundo
-        hashrate_hps = (total_generated * 16 ** 8) / wait
-
-        # Convierte el hashrate a Petahashes por segundo (1 PH/s = 10**15 H/s)
+    def count_valid_merkle_roots(self, wait, job_queue, generated_merkle_roots, merkle_roots):
+        total_generated = len(generated_merkle_roots)
+        hashrate_hps = (total_generated * (16 ** 8)) / wait
         hashrate_phps = hashrate_hps / 10 ** 12
 
-        print(
-            f"merkle roots {len(self.merkle_roots_done)}:{len(self.merkle_roots)} de {total_generated} (Hashrate: {hashrate_phps:.5f} TH/s)")
-        # print(f"merkle roots válidos en {wait} segundos: {valid_count} de {total_generated} (Hashrate: {hashrate_phps:.5f} TH/s)")
-        self.generated_merkle_roots = []
-        self.merkle_roots = []
-        self.merkle_roots_done = []
+        print(f"merkle roots {len(self.merkle_roots_done)}:{len(merkle_roots)} de {total_generated} (Hashrate: {hashrate_phps:.5f} TH/s)")
+        self.generated_merkle_roots[:] = []
+        self.merkle_roots[:] = []
+        self.merkle_roots_done[:] = []
 
-    def job_generator(self):
+    def job_generator(self, job_queue, generated_merkle_roots, merkle_roots):
         while True:
             try:
-                jobs = self.stratum_processing.generate_jobs()
+                jobs, gen_mr, mr = self.stratum_processing.generate_jobs()
                 if jobs:
-                    self.job_queue.extend(jobs)
+                    job_queue.extend(jobs)
+                    generated_merkle_roots.extend(gen_mr)
+                    merkle_roots.extend(mr)
             except Exception as e:
                 logging.error(f"Error en job_generator: {e}")
                 time.sleep(1)
@@ -89,31 +83,47 @@ class MinerCPU:
             if self.job_queue:
                 job = self.job_queue.pop(0)
                 if job:
-                    # Procesar el diccionario directamente
                     self.stratum_processing.process_job_opencl(job)
             else:
                 time.sleep(0.1)
 
     def start(self):
         threading.Thread(target=self.block_template_updater, daemon=True).start()
-        threading.Thread(target=self.merkle_root_counter, daemon=True).start()
+        # threading.Thread(target=self.merkle_root_counter, daemon=True).start()
+        counter_processes = [
+            multiprocessing.Process(target=self.merkle_root_counter,
+                                    args=(self.job_queue, self.generated_merkle_roots, self.merkle_roots))
+            for _ in range(1)
+        ]
+        for process_c in counter_processes:
+            process_c.start()
+
         time.sleep(3)
-        threading.Thread(target=self.job_generator, daemon=True).start()
-        # with ThreadPoolExecutor(max_workers=100) as executor:
-        #     for _ in range(1000):  # Ajustar el número de hilos productores
-        #         executor.submit(self.job_generator)
-        # with ThreadPoolExecutor(max_workers=100) as executor:
-        #     for _ in range(100):  # Ajustar el número de hilos productores
-        #         executor.submit(self.job_processor)
-        self.job_processor()
+
+        num_generators = 4
+        generator_processes = [
+            multiprocessing.Process(target=self.job_generator, args=(self.job_queue, self.generated_merkle_roots, self.merkle_roots))
+            for _ in range(num_generators)
+        ]
+
+        for process in generator_processes:
+            process.start()
+
+        try:
+            self.job_processor()
+        finally:
+            for process in generator_processes:
+                process.terminate()
+                process.join()
+            for process_c in counter_processes:
+                process_c.terminate()
+                process_c.join()
 
 
 class StratumProcessing:
     def __init__(self, config, proxy):
         self.config = config
         self.proxy = proxy
-        self.db_file = config.get('DATABASE', 'db_file')
-        self.db_handler = DatabaseHandler(self.db_file)
         self.block_template_fetcher = BlockTemplate(config)
         self.block_template = None
         self.coinbase_message = None
@@ -128,7 +138,6 @@ class StratumProcessing:
         self.jobs = {}
 
     def process_job_opencl(self, job):
-
         ntime = job['ntime']
         nbits = job['nbits']
         prevhash = job['prevhash']
@@ -138,32 +147,24 @@ class StratumProcessing:
         merkle_branch = job['merkle_branch']
         merkle_root = job['merkle_root']
 
-        # if merkle_root not in self.proxy.merkle_roots:
         nonce_df = self.check_merkle_nonce(merkle_root)
         if nonce_df is not None:
             self.proxy.merkle_roots_done.append(merkle_root)
             for nonce_one in nonce_df:
-
                 nonce_init = int(nonce_one.ljust(8, '0'), 16)
                 len_nonce = 8 - len(nonce_one)
                 nonce_end = nonce_init + (16 ** len_nonce)
 
-                # Generar todos los nonces posibles
                 nonces = [f"{nonce:08x}" for nonce in range(nonce_init, nonce_end)]
-
-                # Crear todos los headers posibles
                 headers = [
                     version + prevhash + merkle_root + ntime + nbits + nonce
                     for nonce in nonces
                 ]
-                # Calcular los hashes usando OpenCL
                 hashes = sha256_pyopencl(headers, num_zeros=NUM_ZEROS)
                 target = int(self.bits_to_target(job['nbits']), 16)
 
-                # Comprobar si algún hash cumple con el objetivo
                 for i in range(len(hashes)):
                     hash_hex = hashes[i]['hash']
-                    print(hash_hex)
                     if int(hash_hex, 16) < target:
                         header = hashes[i]['data']
                         print(f"blockhash: {hash_hex}")
@@ -173,50 +174,11 @@ class StratumProcessing:
                         except requests.exceptions.RequestException as e:
                             print(f"Error al enviar el bloque: {e}")
 
-    # def process_job_cpu(self, job):
-    #     ntime = job['ntime']
-    #     nbits = job['nbits']
-    #     prevhash = job['prevhash']
-    #     version = job['version']
-    #     coinbase1 = job['coinbase1']
-    #     coinbase2 = job['coinbase2']
-    #     merkle_branch = job['merkle_branch']
-    #     merkle_root = job['merkle_root']
-    #
-    #     # self.proxy.merkle_roots.append(merkle_root)
-    #     nonce_df = self.check_merkle_nonce(merkle_root)
-    #     if nonce_df is not None:
-    #         self.proxy.merkle_roots_done.append(merkle_root)
-    #         for nonce_one in nonce_df:
-    #
-    #
-    #             nonce_init = int(nonce_one.ljust(8, '0'), 16)
-    #             len_nonce = 8 - len(nonce_df)
-    #             nonce_end = nonce_init + (16 ** len_nonce)
-    #
-    #             for nonce in range(nonce_init, nonce_end):
-    #                 nonce_hex = f"{nonce:08x}"
-    #                 block_header = version + prevhash + merkle_root + ntime + nbits + nonce_hex
-    #                 block_hash = hashlib.sha256(
-    #                     hashlib.sha256(bytes.fromhex(block_header)).digest()
-    #                 ).digest().hex()
-    #                 target = int(self.bits_to_target(job['nbits']), 16)
-    #                 target = '{:064x}'.format(target)
-    #                 if int(block_hash, 16) < int(target, 16):
-    #                     print(f"blockhash: {block_hash}")
-    #                     try:
-    #                         response = self.block_template_fetcher.submit_block(block_header)
-    #                         print(f"Respuesta del servidor RPC: {response}")
-    #                     except requests.exceptions.RequestException as e:
-    #                         print(f"Error al enviar el bloque: {e}")
-
     def check_merkle_nonce(self, merkle_root):
-        # Filtrar todas las coincidencias donde el 'merkle_root' dado está al inicio de los valores en la columna 'merkle_root'
         matches = self.proxy.merkle_counts[
             self.proxy.merkle_counts['merkle_root'].apply(lambda x: merkle_root.startswith(x))]
 
         if not matches.empty:
-            # Obtener todos los valores de 'nonce' como una lista
             nonces = matches['nonce'].tolist()
             return nonces
         return []
@@ -227,21 +189,24 @@ class StratumProcessing:
         version = self.version_to_hex(self.block_template['version'])
         nbits = self.block_template['bits']
         ntime = self.to_little_endian(self.int2lehex(int(time.time()), 4))
-        jobs = self.create_job_from_blocktemplate(version, prevhash, nbits, ntime)
-        return jobs
+        jobs, gen_mr, mr = self.create_job_from_blocktemplate(version, prevhash, nbits, ntime)
+        return jobs, gen_mr, mr
 
     def update_block_template(self):
         template = self.block_template_fetcher.get_template()
         self.block_template = template
         if self.height != self.block_template['height']:
-            self.proxy.merkle_roots = []
-            self.proxy.job_queue = []
+            self.proxy.merkle_roots[:] = []
+            self.proxy.job_queue[:] = []
+            self.proxy.generated_merkle_roots[:] = []
+            self.proxy.merkle_roots_done[:] = []
         self.height = self.block_template['height']
 
-    def worker_job(self, proxy, version, prev_block, nbits, ntime):
-        # while True:
+    def worker_job(self, version, prev_block, nbits, ntime):
         jobs = []
-        for i in range(1000):
+        gen_mr = []
+        mr = []
+        for i in range(100000):
             self.transactions = self.select_random_transactions()
             extranonce_placeholder = "00000000"
             miner_message = self.miner_message()
@@ -257,9 +222,9 @@ class StratumProcessing:
                     raise ValueError("Cada transacción debe contener 'hash' y 'data'")
             merkle_branch = self.compute_merkle_branch(merkle_hashes)
             merkle_root_candidate = self.compute_merkle_root(merkle_hashes)
-            proxy.generated_merkle_roots.append(merkle_root_candidate)
+            gen_mr.append(merkle_root_candidate)
             if self.check_merkle_root(merkle_root_candidate):
-                proxy.merkle_roots.append(merkle_root_candidate)
+                mr.append(merkle_root_candidate)
 
                 job = {
                     'version': self.to_little_endian(version),
@@ -273,28 +238,18 @@ class StratumProcessing:
                     'height': self.height,
                 }
                 jobs.append(job)
-        return jobs
+        return jobs, gen_mr, mr
 
     def create_job_from_blocktemplate(self, version, prevhash, nbits, ntime):
         valid_jobs = []
-        # stop_event = threading.Event()
-        # height = self.height
-
-        # with ThreadPoolExecutor(max_workers=100) as executor:
-        #     futures = [executor.submit(self.worker_job, self.proxy, version, prevhash, nbits, ntime) for _ in range(10000)]
-        #     for future in concurrent.futures.as_completed(futures):
-        #         job = future.result()
-        #         if job is not None:
-        #             valid_jobs.append(job)
-        #             break
-        jobs = self.worker_job(self.proxy, version, prevhash, nbits, ntime)
+        generated_merkles = []
+        merkles = []
+        jobs, gen_mr, mr = self.worker_job(version, prevhash, nbits, ntime)
         if jobs is not None:
             valid_jobs.extend(jobs)
-            # for job in jobs:
-            #     valid_jobs.append(job)
-        # if height != self.block_template['height']:
-        #     return None
-        return valid_jobs
+        generated_merkles.extend(gen_mr)
+        merkles.extend(mr)
+        return valid_jobs, generated_merkles, merkles
 
     def compute_merkle_branch(self, merkle):
         branches = []
@@ -343,28 +298,28 @@ class StratumProcessing:
         block_height = self.height
         coinbase_script = self.create_coinbase_script(block_height, miner_message, extranonce_placeholder)
 
-        max_coinbase_script_size = 100 * 2  # 100 bytes in hex characters
+        max_coinbase_script_size = 100 * 2
         actual_script_size = len(coinbase_script) + len(extranonce_placeholder)
         if actual_script_size > max_coinbase_script_size:
             coinbase_script = coinbase_script[:max_coinbase_script_size - len(extranonce_placeholder)]
 
         coinbase1 = (
-                "02000000"  # Transacción de versión
-                + "01"  # Número de entradas
-                + "0" * 64  # Hash previo (32 bytes de ceros)
-                + "ffffffff"  # Índice previo (4 bytes de f's)
+                "02000000"
+                + "01"
+                + "0" * 64
+                + "ffffffff"
                 + self.int2varinthex((len(coinbase_script) + len(extranonce_placeholder)) // 2)
                 + coinbase_script
                 + extranonce_placeholder
         )
         self.fee = self.calculate_coinbase_value()
         coinbase2 = (
-                "ffffffff"  # Secuencia (4 bytes de f's)
-                + "01"  # Número de salidas
-                + self.int2lehex(self.fee, 8)  # Recompensa en little-endian (8 bytes)
-                + self.int2varinthex(len(pubkey_script) // 2)  # Longitud del pubkey_script
-                + pubkey_script  # Script público
-                + "00000000"  # Bloque de tiempo de lock (4 bytes de ceros)
+                "ffffffff"
+                + "01"
+                + self.int2lehex(self.fee, 8)
+                + self.int2varinthex(len(pubkey_script) // 2)
+                + pubkey_script
+                + "00000000"
         )
 
         return coinbase1, coinbase2
@@ -460,7 +415,7 @@ def start_miner(config_path):
 
 def main():
     config_path = 'config.ini'
-    num_processes = 1  # Número de procesos que deseas ejecutar
+    num_processes = 1
 
     processes = []
 
@@ -469,7 +424,6 @@ def main():
         p.start()
         processes.append(p)
 
-    # Esperar a que todos los procesos terminen
     for p in processes:
         p.join()
 
